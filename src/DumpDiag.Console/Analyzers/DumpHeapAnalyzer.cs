720082bddb7204aebb1b24accc71d5ba8e5a3aaa
Pavel Krymets <pavel@krymets.com>
2018-11-09 12:47:32 -0800

    Dump isntances

---
+++ src/DumpDiag.Console/Analyzers/DumpHeapAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace DumpDiag.Console
+{
+    internal class DumpHeapAnalyzer : IDumpAnalyzer
+    {
+        public void Run(AnalysisSession analysisSession)
+        {
+            foreach (var o in analysisSession.Context.Runtime.Heap.EnumerateObjects().Take(50))
+            {
+                if (o.Type.IsString)
+                {
+                    analysisSession.Reporter.Write(new StringInstance((string)o.Type.GetValue(o.Address)));
+                }
+                else
+                {
+                    analysisSession.Reporter.Write(new ObjectInstance(o));
+                }
+            }
+        }
+    }
+}
