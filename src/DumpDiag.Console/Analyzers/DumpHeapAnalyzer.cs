1c0f8cd975584d6b0f63b858276396e3eae8a126
Pavel Krymets <pavel@krymets.com>
2018-11-09 14:02:55 -0800

    Save

--- src/DumpDiag.Console/Analyzers/DumpHeapAnalyzer.cs
+++ src/DumpDiag.Console/Analyzers/DumpHeapAnalyzer.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Globalization;
 using System.Linq;
-using Microsoft.Diagnostics.Runtime;
 
 namespace DumpDiag.Console
 {
-    internal class DumpObjAnalyzer : IDumpAnalyzer
-    {
-        public void Run(AnalysisSession analysisSession, string[] arguments)
-        {
-            var address = ulong.Parse(arguments[0], NumberStyles.HexNumber);
-            var type = analysisSession.Context.Runtime.Heap.GetObjectType(address);
-            analysisSession.Reporter.Write(new ClrObject(address, type).Describe());
-        }
-    }
-
     internal class DumpHeapAnalyzer : IDumpAnalyzer
     {
         public void Run(AnalysisSession analysisSession, string[] arguments)
